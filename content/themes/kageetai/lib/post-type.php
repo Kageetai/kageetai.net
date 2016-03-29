3ff6aeb6bc0b3b76e2d5408cdb6ed9ad181772ac
Kageetai <ma@kageetai.net>
2016-03-29 14:51:23 +0200

    added portfolio custom post type and bootstrap nav walker support

---
+++ content/themes/kageetai/lib/post-type.php
@@ -0,0 +1,55 @@
+<?php
+// Register Custom Post Type
+function Portfolio() {
+
+$labels = array(
+'name'                  => 'Portfolios',
+'singular_name'         => 'Portfolio',
+'menu_name'             => 'Portfolio',
+'name_admin_bar'        => 'Portfolio',
+'archives'              => 'Portfolio Archives',
+'parent_item_colon'     => 'Parent Item:',
+'all_items'             => 'All Items',
+'add_new_item'          => 'Add New Item',
+'add_new'               => 'Add New',
+'new_item'              => 'New Item',
+'edit_item'             => 'Edit Item',
+'update_item'           => 'Update Item',
+'view_item'             => 'View Item',
+'search_items'          => 'Search Item',
+'not_found'             => 'Not found',
+'not_found_in_trash'    => 'Not found in Trash',
+'featured_image'        => 'Featured Image',
+'set_featured_image'    => 'Set featured image',
+'remove_featured_image' => 'Remove featured image',
+'use_featured_image'    => 'Use as featured image',
+'insert_into_item'      => 'Insert into item',
+'uploaded_to_this_item' => 'Uploaded to this item',
+'items_list'            => 'Items list',
+'items_list_navigation' => 'Items list navigation',
+'filter_items_list'     => 'Filter items list',
+);
+$args = array(
+'label'                 => 'Portfolio',
+'description'           => 'Portfolio Items',
+'labels'                => $labels,
+'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'revisions', 'page-attributes', 'post-formats', ),
+'taxonomies'            => array( 'category', 'post_tag' ),
+'hierarchical'          => false,
+'public'                => true,
+'show_ui'               => true,
+'show_in_menu'          => true,
+'menu_position'         => 5,
+'menu_icon'             => 'dashicons-media-code',
+'show_in_admin_bar'     => true,
+'show_in_nav_menus'     => true,
+'can_export'            => true,
+'has_archive'           => true,
+'exclude_from_search'   => false,
+'publicly_queryable'    => true,
+'capability_type'       => 'page',
+);
+register_post_type( 'portfolio', $args );
+
+}
+add_action( 'init', 'Portfolio', 0 );
--- content/themes/kageetai/templates/header.php
+++ content/themes/kageetai/templates/header.php
@@ -4,7 +4,16 @@
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu([
+          'theme_location'    => 'primary_navigation',
+          'depth'             => 2,
+          'container'         => 'div',
+          'container_class'   => 'collapse navbar-collapse',
+          'container_id'      => 'navbar-primary',
+          'menu_class'        => 'nav navbar-nav',
+          'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+          'walker'            => new wp_bootstrap_navwalker()
+        ]);
       endif;
       ?>
     </nav>
