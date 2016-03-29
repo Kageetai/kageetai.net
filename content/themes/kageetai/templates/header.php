9d4062416d7a458dacfd1f86ee8b685b02d13a92
Kageetai <ma@kageetai.net>
2016-03-29 16:57:00 +0200

    first batch of header and navbar styles

--- content/themes/kageetai/templates/header.php
+++ content/themes/kageetai/templates/header.php
@@ -1,7 +1,21 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+
+    <nav class="navbar navbar-default nav-primary" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target="#navbar-primary">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-brand">
+          social
+        </div>
+      </div>
+
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu([
@@ -10,7 +24,7 @@
           'container'         => 'div',
           'container_class'   => 'collapse navbar-collapse',
           'container_id'      => 'navbar-primary',
-          'menu_class'        => 'nav navbar-nav',
+          'menu_class'        => 'nav navbar-nav navbar-right',
           'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
           'walker'            => new wp_bootstrap_navwalker()
         ]);
