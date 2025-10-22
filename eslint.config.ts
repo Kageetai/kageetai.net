b6af24fb0ad9505aa48a666eb2f704863ed97dd2
Kageetai <ma@kageetai.net>
2025-10-22 14:03:08 +0200

    feat: Add ESLint configuration and Prettier integration for code formatting

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import css from "@eslint/css";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx,astro}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  globalIgnores([".astro/**/*"], "Ignore Build Directory"),
+]);
