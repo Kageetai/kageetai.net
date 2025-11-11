3f40aa2ac561c2faf107f14e3e6991ac0a1fc0a7
Michael Seifarth <Kageetai@users.noreply.github.com>
2025-11-11 14:33:41 +0100

    feat(matomo): Add `astro-matomo` integration (#34)
    Integrate `astro-matomo` for Matomo analytics support in production. Update dependencies in `package.json` and `pnpm-lock.yaml` accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import matomo from "astro-matomo";
 import mdx from "@astrojs/mdx";
+
 import { remarkYouTubeEmbed } from "./src/lib/remark-youtube-embed.ts";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://matomo.kageetai.net/",
+      setCookieDomain: "*.kageetai.net",
+      siteId: 1,
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkYouTubeEmbed],
   },
